ec654c7d275c59b51406dc56da96c8f9bd421b86
gtamasteckno <gabriel.tamas@tecknoworks.com>
2017-07-20 13:29:54 +0300

    Add files via upload

--- ACTIVITIES.OBJECTIVES.md
+++ ACTIVITIES.OBJECTIVES.md
@@ -1,2 +1,4 @@
 19.07.2017 - created project structure
 20.07.2017 - created support for multiple rooms
+           - user can create and join different rooms
+		   - cleaned up code
--- app.js
+++ app.js
@@ -6,93 +6,76 @@ var express = require('express')
 
 server.listen(8080);
 
-
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-
 var usernames = {};
-
 
 var rooms = ['room1','room2','room3', 'abc'];
 
 io.sockets.on('connection', function (socket) {
-	
-	
-	socket.on('adduser', function(username){
-		
+		socket.on('adduser', function(username){
 		socket.username = username;
-		
 		socket.room = 'room1';
-		
 		usernames[username] = username;
-		
 		socket.join('room1');
-		
 		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-		
 		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
 		socket.emit('updaterooms', rooms, 'room1');
 	});
 
-
 	socket.on('room',function(newroom){
-
 		var i;
-		for (i=0; i<rooms.lenght; i++)
-			if (rooms[i]== newroom)
-				console.log('room exists');
-			else
-			{
-		rooms.push(newroom);
+		var roomFound=false;
+		console.log(newroom);
+		for (i=0;i<rooms.length;i++){
+			if (rooms[i] == newroom)
+				roomFound=true;
+		}
+		if (roomFound==false)
+		{
+			rooms.push(newroom);
+			socket.room = newroom;
+			socket.leave(socket.room);
+			socket.join(newroom);
+			socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+			socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+			socket.room = newroom;
+			socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+			socket.emit('updaterooms', rooms, newroom);
+		}
+		else
+		{
+			socket.room = newroom;
+			socket.leave(socket.room);
+			socket.join(newroom);
+			socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+			socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+			socket.room = newroom;
+			socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+			socket.emit('updaterooms', rooms, newroom);
+		}
 		
-		socket.room = newroom;
-		
-
-		socket.leave(socket.room);
-		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-		
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-		
-		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-		socket.emit('updaterooms', rooms, newroom);
-	}
-
 	});
 
-	
-	
-	
 	socket.on('sendchat', function (data) {
-		
 		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
 	});
 
-
-	
 	socket.on('switchRoom', function(newroom){
 		socket.leave(socket.room);
 		socket.join(newroom);
 		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-		
 		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-		
 		socket.room = newroom;
 		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
 		socket.emit('updaterooms', rooms, newroom);
 	});
 	
-
-	
 	socket.on('disconnect', function(){
-		
 		delete usernames[socket.username];
-		
 		io.sockets.emit('updateusers', usernames);
-		
 		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
 		socket.leave(socket.room);
 	});
